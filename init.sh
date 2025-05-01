57cbbe88ea7442a99194e95b999eefe4fb716540
Madu-de <kontakt.madu@gmail.com>
2025-05-01 02:03:26 +0200

    Renamed LEIHUGT=>MOTIVATEME

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ echo -e "${Cyan}MOTIVATEME ${Green}[INFO]${Color_Off} | ${Purple}Create 80.conf 
 
 sed "s/__DOMAIN__/$MOTIVATEME_LETS_ENCRYPT_DOMAIN/g" proxy/nginx-80-template.conf > dist/conf.d/80.conf
 
-echo -e "${Cyan}LEIHUGT ${Green}[INFO]${Color_Off} | ${Purple}Start application${Color_Off}"
+echo -e "${Cyan}MOTIVATEME ${Green}[INFO]${Color_Off} | ${Purple}Start application${Color_Off}"
 
 docker compose -f docker-compose.prod.yml up --build -d
 
